e6f553157c07bc1aaaadb6fd884dd3d735b93911
Fred011 <frederic.vannier01@gmail.com>
2021-04-11 21:26:06 +0200

    folder architecture created and new directory component done. Styles started

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+// @flow
+import React from 'react';
+import Directory from '../../components/directory/directory.component';
+import './homepage.scss'
+
+
+const Homepage = () => (
+    <div className='homepage'>
+       <Directory />
+    </div>
+)
+
+export default Homepage;
