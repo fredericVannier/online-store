615cd2bca8aea347222b9d00f4acee11358c5c0e
Fred011 <frederic.vannier01@gmail.com>
2021-04-13 21:43:18 +0200

    react router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
 
-import Homepage from './pages/homepage/homepage.component';
+import Homepage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
